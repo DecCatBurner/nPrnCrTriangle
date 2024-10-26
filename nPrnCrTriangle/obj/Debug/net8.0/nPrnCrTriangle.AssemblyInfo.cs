fe30a11277f76d22463b191833fd9ec5567b66d4
Cat Burner <DecCatBurner@gmail.com>
2024-10-26 13:49:22 -0500

    Update syntax, then take it back since quizbowl's incorrect use of rows also ruins the underlying pattern

--- nPrnCrTriangle/obj/Debug/net8.0/nPrnCrTriangle.AssemblyInfo.cs
+++ nPrnCrTriangle/obj/Debug/net8.0/nPrnCrTriangle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nPrnCrTriangle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1575a7d96071d92af28ebaede3dc8c603b0df1cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("nPrnCrTriangle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nPrnCrTriangle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
